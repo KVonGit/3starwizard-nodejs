0fc5888004ee078865b90b044ddf1c494625766f
K.V. <30656341+KVonGit@users.noreply.github.com>
2025-04-05 02:02:28 -0500

    crazypass
    - Use the super-dooper-looper-crazy password encryption

--- streak-tracker/src/server.js
+++ streak-tracker/src/server.js
@@ -5,9 +5,9 @@ const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Create data directory and streakdata.json file if they don't exist
+// Create data directory and streaks.json file if they don't exist
 const dataDir = path.join(__dirname, 'data');
-const dataFile = path.join(dataDir, 'streakdata.json');
+const dataFile = path.join(dataDir, 'streaks.json');
 
 if (!fs.existsSync(dataDir)) {
     fs.mkdirSync(dataDir);
@@ -26,9 +26,7 @@ app.use('/api', require('./routes/api'));
 
 // Serve the main HTML page
 app.get('/', (req, res) => {
-    res.render('index', { 
-        password: process.env.THREE_STAR_PASSWORD 
-    });
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Start the server
